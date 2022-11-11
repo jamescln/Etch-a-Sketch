3aa32a7444c522dc838f60b28b1c601d1c3d0611
James <james.jcarline@gmail.com>
2022-11-11 21:41:10 +0000

    Create sketch effect
    Add event listener to etchBox divs. Add etch-box-active class to CSS.

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const createGrid = function () {
   for (let i = 0; i < 256; i++) {
     const etchBox = document.createElement("div");
     etchBox.className = "etch-box";
+    etchBox.onmouseover = (event) => {
+      event.target.className = "etch-box-active";
+    };
     container.appendChild(etchBox);
   }
 };
