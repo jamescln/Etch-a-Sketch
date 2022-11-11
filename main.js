345f3e91e05f6d1471158fb1797d05bb1c849f7f
James <james.jcarline@gmail.com>
2022-11-11 23:17:29 +0000

    Fix createNewGrid button and function
    The createNewGrid function created the right amount of elements but they would only display in a 16 x 16
    no matter how big the gridSize variable was. createNewGrid now also alters the CSS grid layout of
    .container to accomodate the correct amount of elements.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // Global variables
 const container = document.querySelector(".container");
-let gridSize = 256;
+const newGridbtn = document.querySelector(".newGridButton");
+let gridSize = 16;
 
 // Global functions
 const createGrid = function () {
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const etchBox = document.createElement("div");
     etchBox.className = "etch-box";
     etchBox.onmouseover = (event) => {
@@ -15,15 +16,23 @@ const createGrid = function () {
 };
 
 const createNewGrid = function () {
-  for (let i = 0; i < gridSize; i++) {
-    container.removeChild(etchBox);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    container.removeChild(container.lastChild);
   }
   gridSize = Number(prompt("How big do you want the grid? (From 10 to 100)."));
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  if (gridSize >= 10 && gridSize <= 100) {
+    createGrid();
+  } else {
+    console.log(gridSize);
+  }
 };
-if (gridSize >= 10 && gridSize <= 100) {
-  createGrid();
-}
 
 // Other
+
+newGridbtn.onclick = (event) => {
+  createNewGrid();
+};
 
 createGrid();
